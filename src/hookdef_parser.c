ddc411ee5132b761d76d33234e14645ce2d70393
Clif Wolfe <clifwolfe1@gmail.com>
2025-06-28 19:32:11 -0400

    added stubs for parsers

--- src/hookdef_parser.c
+++ src/hookdef_parser.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <strings.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "hookdef_parser.h"
 #include "intrusive_list.h"
 #include "debug_utils.h"
@@ -37,12 +38,8 @@ struct HookdefParseReq {
     int nargs;
 };
 
-
-
 static struct list_head hook_def_head = LIST_HEAD_INIT(hook_def_head);
-
 static struct list_head sym_list = LIST_HEAD_INIT(sym_list);
-
 
 static void add_sym(char* name, uint64_t addr) {
     struct SymData* dat = (struct SymData*)malloc(sizeof(struct SymData));
@@ -68,16 +65,45 @@ struct HookDef* parse_hookdef_args(int argc, char** argv) {
     return res;
 }
 
+struct HookDef* parse_hookdef_regs(int argc, char** argv) {
+    struct HookDef* res = NULL;
+    printf("parse regs\n");
+    return res;
+}
+
+struct HookDef* parse_hookdef_regaddr(int argc, char** argv) {
+    struct HookDef* res = NULL;
+    printf("parse regaddr\n");
+    return res;
+}
+
+struct HookDef* parse_hookdef_allregs(int argc, char** argv) {
+    struct HookDef* res = NULL;
+    printf("parse allregs\n");
+    return res;
+}
+
+struct HookDef* parse_hookdef_dumpaddr(int argc, char** argv) {
+    struct HookDef* res = NULL;
+    printf("parse dumpaddr\n");
+    return res;
+}
+
+
 static struct HookdefParseReq parsereqs[] = {
     [HOOKDEF_NONE] =     { .token = "", .nargs = -1},
     [HOOKDEF_ARGS] =     { .token = "args", .nargs = 2,        // <addr|sym>,<num-regs>
                            .parse_func = &parse_hookdef_args},
-    [HOOKDEF_REGS] =     { .token = "regs", .nargs = 2},       // <addr|sym>,<reglist>
-    [HOOKDEF_REGADDR] =  { .token = "regaddr", .nargs = 3},    // <addr|sym>,<reg>,<size>
+    [HOOKDEF_REGS] =     { .token = "regs", .nargs = 2,
+                           .parse_func = &parse_hookdef_regaddr},       // <addr|sym>,<reglist>
+    [HOOKDEF_REGADDR] =  { .token = "regaddr", .nargs = 3,
+                           .parse_func = &parse_hookdef_regaddr},    // <addr|sym>,<reg>,<size>
     [HOOKDEF_SYM] =      { .token = "sym", .nargs = 2,
                            .parse_func = &parse_hookdef_sym},  // <name>,<addr>
-    [HOOKDEF_ALLREGS] =  { .token = "allregs", .nargs = 1},    // <addr|sym>
-    [HOOKDEF_DUMPADDR] = { .token = "dumpaddr", .nargs = 3},   // <addr|sym>,<addr|sym>,<size>
+    [HOOKDEF_ALLREGS] =  { .token = "allregs", .nargs = 1,
+                           .parse_func = &parse_hookdef_allregs},    // <addr|sym>
+    [HOOKDEF_DUMPADDR] = { .token = "dumpaddr", .nargs = 3,
+                           .parse_func = &parse_hookdef_dumpaddr},   // <addr|sym>,<addr|sym>,<size>
     {.token = NULL, .nargs = -1}
 };
 
@@ -85,9 +111,12 @@ static struct HookdefParseReq parsereqs[] = {
 void parse_hookdef_cmd(int argc, char** argv){
     int currind = 0;
     struct HookDef* hook_def;
-    while (currind <= argc-1 && argv[currind] != NULL) {
+    struct HookdefParseReq* parsereq;
+    while ((currind < argc-1) && argv[currind] != NULL) {
+        //printf("currind %d argc-1 %d\n", currind, argc-1);
+        //fflush(stdout);
         hook_def = NULL;
-        struct HookdefParseReq* parsereq = NULL;
+        parsereq = NULL;
         for (size_t i = 0; i < sizeof(parsereqs)/sizeof(struct HookdefParseReq); i++) {
             // +1 to handle cases where the start of a command name is the same as the entire name of another command
             if (0 == strncasecmp(parsereqs[i].token, argv[currind], strlen(parsereqs[i].token)+1)) {
@@ -99,12 +128,14 @@ void parse_hookdef_cmd(int argc, char** argv){
         currind++;
         if (parsereq == NULL) {
             printf("invalid hook type %s\n", argv[currind-1]);
+            fflush(stdout);
             continue;
         }
 
         if (parsereq->nargs + currind > argc) {
             printf("not enough args to parse %s\n", argv[currind-1]);
-            continue;
+            fflush(stdout);
+            break;
         }
 
         if (parsereq->parse_func != NULL) {
