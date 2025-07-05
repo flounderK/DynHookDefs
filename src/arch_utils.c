a10856aeb0379683d57fa93293424c9e1a8157a6
Clif Wolfe <clifwolfe1@gmail.com>
2025-07-05 11:00:53 -0400

    added arch utils registration

--- src/arch_utils.c
+++ src/arch_utils.c
@@ -1,3 +1,13 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "arch_utils.h"
+
+static struct ArchOps* ops = NULL;
+
+void register_arch_ops(struct ArchOps* new_ops) {
+    ops = new_ops;
+}
+
+struct ArchOps* get_arch_ops() {
+    return ops;
+}
--- src/arch_utils.h
+++ src/arch_utils.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 // Singleton Run Time interface for interacting with arch-specific things
-struct ArchUtilsOps {
+struct ArchOps {
     uint32_t (*regname_to_regno)(char*);
     const char* (*regno_to_reg_name)(uint32_t);
     // can't do argno to regno because some architectures pass args past 3 on the stack
@@ -12,5 +12,8 @@ struct ArchUtilsOps {
     uint64_t (*get_return_pc)(void*);
 
 };
+
+void register_arch_ops(struct ArchOps* new_ops);
+struct ArchOps* get_arch_ops();
 
 #endif //__ARCH_UTILS_H__
